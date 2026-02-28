e8c2d6762c2304153a3cf4b76792b5cc212bda72
ssssss222www <15323379312@163.com>
2026-02-28 14:11:58 +0800

    Remove notebooks from tracking

---
+++ pipelines/run_nanopolish/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## convert single-fast5 to multi-fast5
+bash /mnt/TWET-20250901A/YMM/scripts/guppy/convet_multi.sh
+
+## bascalling
+bash /mnt/TWET-20250901A/YMM/scripts/guppy/basecalling.sh
+
